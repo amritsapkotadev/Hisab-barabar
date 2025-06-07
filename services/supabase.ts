b1c543a97c19eada8d3ea6031289230d1a29bb4f
Amrit <amritsapkota.dev@gmail.com>
2025-06-07 14:56:42 +0545

    commit

--- services/supabase.ts
+++ services/supabase.ts
@@ -7,7 +7,7 @@ import { Platform } from 'react-native';
 const supabaseUrl = 'https://fhrkdeejcqqfofgsiluf.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZocmtkZWVqY3FxZm9mZ3NpbHVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NDA1NTEsImV4cCI6MjA2NDExNjU1MX0.5_z4ilJ4z-KwvZm21bMOdZW2XWxDHgK0jKzmCWO-ORU';
 
-// ✅ Fixed adapter — removed stray `z`
+
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => SecureStore.getItemAsync(key),
   setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
